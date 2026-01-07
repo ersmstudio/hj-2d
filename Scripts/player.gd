7250c4ea13ed4b7dedaef89914949506b37bec97
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-07 18:49:29 +0200

    UPDATE ( ShadowEXE )!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,16 +4,22 @@ extends CharacterBody2D
 @export var Jump: float = -450.0
 @export var Platform: bool = false
 
-@onready var TDCollison: CollisionShape2D = $TopDownCollision
-@onready var PMCollison: CollisionShape2D = $PlatformCollision
+@onready var TDCollision: CollisionShape2D = $TopDownCollision
+@onready var PMCollision: CollisionShape2D = $PlatformCollision
 @onready var PlayerAnim: AnimatedSprite2D = $AnimatedSprite2D
 
 var attacking := false
 var in_air := false
 
 func _physics_process(delta: float) -> void:
-	TDCollison.disabled = Platform
-	PMCollison.disabled = not Platform
+	TDCollision.disabled = Platform
+	PMCollision.disabled = not Platform
+
+	handle_attack()
+
+	if attacking:
+		move_and_slide()
+		return
 
 	if Platform:
 		platform_move(delta)
@@ -22,31 +28,30 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func top_down_move() -> void:
-	var dir := Vector2(
-		Input.get_axis("Left", "Right"),
-		Input.get_axis("Up", "Down")
-	)
-
+func handle_attack() -> void:
 	if Input.is_action_just_pressed("Attack") and not attacking:
 		attacking = true
 		velocity = Vector2.ZERO
 		PlayerAnim.play("Hit")
 		await PlayerAnim.animation_finished
 		attacking = false
-		return
 
-	if attacking:
-		return
+func top_down_move() -> void:
+	var dir := Vector2(
+		Input.get_axis("Left", "Right"),
+		Input.get_axis("Up", "Down")
+	)
 
 	if dir != Vector2.ZERO:
 		velocity = dir.normalized() * Speed
-		PlayerAnim.play("Walk")
+		if PlayerAnim.animation != "Walk":
+			PlayerAnim.play("Walk")
 		if dir.x != 0:
 			PlayerAnim.flip_h = dir.x < 0
 	else:
 		velocity = Vector2.ZERO
-		PlayerAnim.play("Idle")
+		if PlayerAnim.animation != "Idle":
+			PlayerAnim.play("Idle")
 
 func platform_move(delta: float) -> void:
 	var dir_x := Input.get_axis("Left", "Right")
@@ -55,6 +60,7 @@ func platform_move(delta: float) -> void:
 	if is_on_floor():
 		if in_air:
 			in_air = false
+
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = Jump
 			in_air = true
