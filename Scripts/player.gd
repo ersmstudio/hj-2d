c69601b5ed5bc78c7b5eea32a2cceff01a17f547
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-07 02:02:31 +0200

    UPDATE ( ShadowEXE )!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,41 +1,30 @@
 extends CharacterBody2D
 
-enum State {
-	IDLE,
-	RUN,
-	Walk,
-	HIT,
-	JUMP,
-	TAKEDAMAGE,
-	DEAD
-	
-}
+@export var SPEED := 300.0
+@onready var PlayerAnim: AnimatedSprite2D = $AnimatedSprite2D
 
+var attacking := false
 
+func _physics_process(delta):
+	var dir := Vector2(
+		Input.get_axis("Left", "Right"),
+		Input.get_axis("Up", "Down")
+	)
 
+	if Input.is_action_just_pressed("Attack") and not attacking:
+		attacking = true
+		PlayerAnim.play("Hit")
+		await PlayerAnim.animation_finished
+		attacking = false
 
-@export_category("Stats")
-@export var speed: int =400
+	if not attacking:
+		if dir != Vector2.ZERO:
+			velocity = dir.normalized() * SPEED
+			PlayerAnim.play("Walk")
+			if dir.x != 0:
+				PlayerAnim.flip_h = dir.x < 0
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+			PlayerAnim.play("Idle")
 
-var state:State = State.IDLE
-
-
-var move_direction: Vector2 =Vector2(0,0)
-
-
-func _physics_process(_delta: float) -> void:
-	movement_loop()
-
-@onready var animation_tree: AnimationTree= $AnimationTree
-@onready var animation_playback: AnimationNodeStateMachine= $AnimationTree["parameters/playback"]
-
-func movement_loop() -> void:
-	move_direction.x = int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
-	move_direction.y = int(Input.is_action_pressed("Down")) - int(Input.is_action_pressed("Up"))
-	var motion: Vector2= move_direction.normalized()* speed
-	set_velocity(motion)
 	move_and_slide()
-	
-	
-	if motion != Vector2.ZERO and state ==State.IDLE:
-		state =State.RUN
