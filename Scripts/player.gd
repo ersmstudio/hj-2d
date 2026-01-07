20b2f13904bd9a5c17c9c8ba4dbb255e014a45a8
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-07 15:23:37 +0200

    UPDATE ( ShadowEXE )!

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 
-@export var SPEED := 300.0
+@export var Speed: float = 300.0
+@export var Jump: float = -450.0
+@export var Platform: bool = false
+
+@onready var TDCollison: CollisionShape2D = $TopDownCollision
+@onready var PMCollison: CollisionShape2D = $PlatformCollision
 @onready var PlayerAnim: AnimatedSprite2D = $AnimatedSprite2D
 
 var attacking := false
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+	TDCollison.disabled = Platform
+	PMCollison.disabled = not Platform
+
+	if Platform:
+		platform_move(delta)
+	else:
+		top_down_move()
+
+	move_and_slide()
+
+func top_down_move() -> void:
 	var dir := Vector2(
 		Input.get_axis("Left", "Right"),
 		Input.get_axis("Up", "Down")
@@ -13,18 +29,37 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("Attack") and not attacking:
 		attacking = true
+		velocity = Vector2.ZERO
 		PlayerAnim.play("Hit")
 		await PlayerAnim.animation_finished
 		attacking = false
+		return
 
-	if not attacking:
-		if dir != Vector2.ZERO:
-			velocity = dir.normalized() * SPEED
+	if attacking:
+		return
+
+	if dir != Vector2.ZERO:
+		velocity = dir.normalized() * Speed
+		PlayerAnim.play("Walk")
+		if dir.x != 0:
+			PlayerAnim.flip_h = dir.x < 0
+	else:
+		velocity = Vector2.ZERO
+		PlayerAnim.play("Idle")
+
+func platform_move(delta: float) -> void:
+	var dir_x := Input.get_axis("Left", "Right")
+	velocity.x = dir_x * Speed
+
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
+		PlayerAnim.play("Jump")
+	else:
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = Jump
+			PlayerAnim.play("Jump")
+		elif dir_x != 0:
 			PlayerAnim.play("Walk")
-			if dir.x != 0:
-				PlayerAnim.flip_h = dir.x < 0
+			PlayerAnim.flip_h = dir_x < 0
 		else:
-			velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 			PlayerAnim.play("Idle")
-
-	move_and_slide()
