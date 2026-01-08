171830058d5f6e0b9dd50e70bc4ade15cf59d4b1
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-08 02:11:24 +0200

    UPDATE FROM ( @ShadowEXE20202 )

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,102 +3,107 @@ extends CharacterBody2D
 @export var Speed: float = 300.0
 @export var Jump: float = -450.0
 @export var Platform: bool = false
+@export var AttackCooldown: float = 0.5  # مدة الهجوم بالثواني
 
 @onready var TDCollision: CollisionShape2D = $TopDownCollision
 @onready var PMCollision: CollisionShape2D = $PlatformCollision
 @onready var WUCollision: CollisionShape2D = $WalkupCollision
-@onready var WUAnim: AnimatedSprite2D = $WalkUp
+@onready var WUAnim: AnimatedSprite2D = $AnimatedSprite2D2
 @onready var PlayerAnim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var Shadow: Sprite2D = $Shadow
 
 var attacking := false
-var in_air := false
 var last_dir := Vector2.DOWN
+var attack_timer := 0.0
 
-const WALK_UP_POS := Vector2(-7.774, -55.898)
-const WALK_DOWN_POS := Vector2(-5.547, 105.0875)
+func _ready() -> void:
+	if Platform:
+		PlayerAnim.visible = true
+		WUAnim.visible = false
+		PlayerAnim.play("Idle")
+	else:
+		PlayerAnim.visible = false
+		WUAnim.visible = true
+		WUAnim.play("IdleUp")
 
 func _physics_process(delta: float) -> void:
 	TDCollision.disabled = Platform
 	WUCollision.disabled = Platform
 	PMCollision.disabled = not Platform
 
-	handle_attack()
+	if attack_timer > 0:
+		attack_timer -= delta
 
 	if attacking:
-		move_and_slide()
+		if Platform:
+			move_and_slide()
+		return
+
+	if Input.is_action_just_pressed("Attack") and not attacking and attack_timer <= 0:
+		await handle_attack()
 		return
 
 	if Platform:
-		platform_move(delta)
+		handle_platform(delta)
 	else:
-		top_down_move()
+		handle_top_down()
 
 	move_and_slide()
 
 func handle_attack() -> void:
-	if Input.is_action_just_pressed("Attack") and not attacking:
-		attacking = true
-		velocity = Vector2.ZERO
-		PlayerAnim.visible = true
-		WUAnim.visible = false
-		PlayerAnim.position = WALK_DOWN_POS
-		PlayerAnim.play("Hit")
-		await PlayerAnim.animation_finished
-		attacking = false
+	attacking = true
+	attack_timer = AttackCooldown
+	velocity = Vector2.ZERO
+	PlayerAnim.visible = true
+	WUAnim.visible = false
+	PlayerAnim.play("Hit")
+	await PlayerAnim.animation_finished
+	attacking = false
 
-func top_down_move() -> void:
-	var dir := Vector2(
-		Input.get_axis("Left", "Right"),
-		Input.get_axis("Up", "Down")
-	)
+func handle_top_down() -> void:
+	var dir := Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
 
 	if dir != Vector2.ZERO:
 		last_dir = dir
 		velocity = dir.normalized() * Speed
-
 		if abs(dir.y) > abs(dir.x):
 			PlayerAnim.visible = false
 			WUAnim.visible = true
-			WUAnim.position = WALK_UP_POS
 			WUAnim.play("WalkUp")
 			WUAnim.flip_v = dir.y > 0
 			Shadow.visible = false
 		else:
 			WUAnim.visible = false
 			PlayerAnim.visible = true
-			PlayerAnim.position = WALK_DOWN_POS
 			PlayerAnim.play("Walk")
 			PlayerAnim.flip_h = dir.x < 0
 			Shadow.visible = true
 	else:
 		velocity = Vector2.ZERO
-		if abs(last_dir.y) > abs(last_dir.x):
-			PlayerAnim.visible = false
-			WUAnim.visible = true
-			WUAnim.position = WALK_UP_POS
-			WUAnim.play("IdleUp")
-			WUAnim.flip_v = last_dir.y > 0
-		else:
-			WUAnim.visible = false
-			PlayerAnim.visible = true
-			PlayerAnim.position = WALK_DOWN_POS
-			PlayerAnim.play("Idle")
-			PlayerAnim.flip_h = last_dir.x < 0
+		play_idle()
 
-func platform_move(delta: float) -> void:
+func play_idle() -> void:
+	if abs(last_dir.y) > abs(last_dir.x):
+		PlayerAnim.visible = false
+		WUAnim.visible = true
+		WUAnim.play("IdleUp")
+		WUAnim.flip_v = last_dir.y > 0
+	else:
+		WUAnim.visible = false
+		PlayerAnim.visible = true
+		PlayerAnim.play("Idle")
+		PlayerAnim.flip_h = last_dir.x < 0
+
+func handle_platform(delta: float) -> void:
 	WUAnim.visible = false
 	PlayerAnim.visible = true
-	PlayerAnim.position = WALK_DOWN_POS
 
 	var dir_x := Input.get_axis("Left", "Right")
 	velocity.x = dir_x * Speed
 
 	if is_on_floor():
-		in_air = false
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = Jump
-			in_air = true
 			PlayerAnim.play("Jump")
 		elif dir_x != 0:
 			PlayerAnim.play("Walk")
@@ -106,4 +111,5 @@ func platform_move(delta: float) -> void:
 		else:
 			PlayerAnim.play("Idle")
 	else:
-		velocity.y += get_gravity().y * delta
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+		velocity.y = min(velocity.y, 1000)
