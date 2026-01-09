f199c7d524dbe69134863fdd58bd6aeafe9ad1cc
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-09 08:24:17 +0200

    UPDATE FROM ( @ShadowEXE20202 )

--- Scripts/intro.gd
+++ Scripts/intro.gd
@@ -3,6 +3,6 @@ extends CanvasLayer
 @export var Path: PackedScene
 
 func _ready():
-	$Control/AnimationPlayer.play("Intro")
+	$Control/Panel/AnimationPlayer.play("introP")
 	await get_tree().create_timer(8).timeout
 	get_tree().change_scene_to_packed(Path)
