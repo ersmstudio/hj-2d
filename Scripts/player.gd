fcb48a1bbf2117236c37d0db1a43174adcfa352c
ShadowEXE <151054551+ShadowEXE20202@users.noreply.github.com>
2026-01-15 17:46:45 +0200

    UPDATE FROM ( @ShadowEXE20202 )

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
 @export var Speed: float = 170.0
-@export var Jump: float = -300.0
+@export var JUMP_VELOCITY: float = -300.0
+@export var HOLD_JUMP_FORCE: float = -500.0
 @export var Platform: bool = false
+@export var Camera: bool = true
+@export var jump_time := 0.0
+@export var MAX_JUMP_TIME := 0.2
 
 @onready var TopDownColl: CollisionShape2D = $TDCollision
 @onready var PlatformColl: CollisionShape2D = $PFCollision
 @onready var PlayerAnim: AnimatedSprite2D = $PlayerAnimation
 
 func _physics_process(delta):
+	if Camera:
+		$Camera2D.enabled = true
+	else:
+		$Camera2D.enabled = false
+	
 	if Platform:
 		# ========= PLATFORM MODE =========
 		PlatformColl.disabled = false
@@ -28,7 +37,12 @@ func _physics_process(delta):
 
 		# Jump
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
-			velocity.y = Jump
+			velocity.y = JUMP_VELOCITY
+		
+		elif Input.is_action_pressed("Jump") and not is_on_floor() and velocity.y < 0:
+			velocity.y += HOLD_JUMP_FORCE * delta
+
+
 
 		# Animations (Platform)
 		if not is_on_floor():
--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @export var Speed: float = 170.0
-@export var Jump: float = -300.0
+@export var JUMP_VELOCITY: float = -300.0
+@export var HOLD_JUMP_FORCE: float = -500.0
 @export var Platform: bool = false
+@export var Camera: bool = true
 
 @onready var TopDownColl: CollisionShape2D = $TDCollision
 @onready var PlatformColl: CollisionShape2D = $PFCollision
 @onready var PlayerAnim: AnimatedSprite2D = $PlayerAnimation
 
 func _physics_process(delta):
+	if Camera:
+		$Camera2D.enabled = true
+	else:
+		$Camera2D.enabled = false
+	
 	if Platform:
 		# ========= PLATFORM MODE =========
 		PlatformColl.disabled = false
@@ -27,8 +34,11 @@ func _physics_process(delta):
 			PlayerAnim.flip_h = dir < 0
 
 		# Jump
-		if Input.is_action_just_pressed("Jump P2") and is_on_floor():
-			velocity.y = Jump
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		elif Input.is_action_pressed("Jump") and not is_on_floor() and velocity.y < 0:
+			velocity.y += HOLD_JUMP_FORCE * delta
 
 		# Animations (Platform)
 		if not is_on_floor():
